2866a9fceea3d12589221a124c193eaf4abfd77f
Kirill-Bryansk <k.mashnov@yandex.ru>
2026-08-26 19:24:26 +1000

    Проверил работу discovery-service

--- discovery-server/src/main/java/ru/shortener/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/ru/shortener/DiscoveryServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveriServerApplication {
+public class DiscoveryServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveriServerApplication.class, args);
+        SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 }
