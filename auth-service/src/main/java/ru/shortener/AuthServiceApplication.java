aefea1fd91000a7b21e72af11b120bf807c16f74
Kirill-Bryansk <k.mashnov@yandex.ru>
2026-08-26 18:38:51 +1000

    Добавил аннотации @EnableDiscoveryClient в сервисы

--- auth-service/src/main/java/ru/shortener/AuthServiceApplication.java
+++ auth-service/src/main/java/ru/shortener/AuthServiceApplication.java
@@ -3,8 +3,10 @@ package ru.shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
