af513a1eebf2db9eea53dbac85aa0c742f4abd5a
Kirill-Bryansk <k.mashnov@yandex.ru>
2026-09-03 09:58:24 +1000

    Изменил метод регистрации пользователя

--- auth-service/src/main/java/ru/shortener/service/AuthService.java
+++ auth-service/src/main/java/ru/shortener/service/AuthService.java
@@ -2,8 +2,10 @@ package ru.shortener.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.shortener.controller.AuthRequest;
 import ru.shortener.controller.AuthResponse;
 import ru.shortener.controller.RegisterRequest;
@@ -22,6 +24,7 @@ public class AuthService {
     private final JwtService jwtService;
     private final BCryptPasswordEncoder passwordEncoder;
 
+    @Transactional
     public AuthResponse register(RegisterRequest request) {
 
         if (userRepository.existsByEmail(request.getEmail())) {
@@ -33,7 +36,14 @@ public class AuthService {
         user.setEmail(request.getEmail());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
 
-        userRepository.save(user);
+        //userRepository.save(user); // При гонке может вызвать 500 а нужен 409
+        try { // отлавливает ошибку
+            userRepository.saveAndFlush(user);
+        } catch (DataIntegrityViolationException e) {
+            // Если гонка проиграна ошибка, то пользователь уже существует
+            throw new DuplicateException(" Пользователь с email: " + request.getEmail() + " уже существует");
+        }
+
         log.debug("Пользователь создан: {}", user.getUsername());
 
         String token = jwtService.generateToken(user.getId(), user.getEmail());
